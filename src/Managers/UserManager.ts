c3a83e7a7c60d1f79815a4b75da70c12b4159157
kagchi <yonuxbot@gmail.com>
2023-01-28 19:19:17 +0700

    fix: remove unused type cast

--- src/Managers/UserManager.ts
+++ src/Managers/UserManager.ts
@@ -1,11 +1,11 @@
-import { APIUser, Snowflake } from "discord-api-types/v10";
+import { APIUser } from "discord-api-types/v10";
 import { BaseManager } from "./BaseManager";
 import { User } from "../Structures/User";
 import { Client } from "../Structures/Client";
 import { RedisCollection } from "@nezuchan/redis-collection";
 import { MakeCacheNameFunction } from "../Utilities/Functions";
 
-export class UserManager extends BaseManager<APIUser & { id: Snowflake }, User> {
+export class UserManager extends BaseManager<APIUser, User> {
     public constructor(
         client: Client
     ) {
@@ -15,12 +15,12 @@ export class UserManager extends BaseManager<APIUser & { id: Snowflake }, User> 
         );
     }
 
-    public override makeCache(): RedisCollection<APIUser & { id: Snowflake }, User> {
+    public override makeCache(): RedisCollection<APIUser, User> {
         return new RedisCollection({
             hash: MakeCacheNameFunction(this.name, this.client.options.clientId!, this.client.options.gatewayRouting ?? false),
             redis: this.client.redis,
             deserialize(value) {
-                return new User(JSON.parse(value as string) as APIUser & { id: Snowflake });
+                return new User(JSON.parse(value as string) as APIUser);
             },
             serialize(value) {
                 return JSON.stringify(value);
@@ -28,7 +28,7 @@ export class UserManager extends BaseManager<APIUser & { id: Snowflake }, User> 
         });
     }
 
-    public override _patch(old: User, data: APIUser & { id: Snowflake }): APIUser & { id: Snowflake } {
+    public override _patch(old: User, data: APIUser): APIUser {
         return data;
     }
 }
