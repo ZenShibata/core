0fc89738ed3856bb227f15b378418fb9861dd34b
KagChi <59391215+KagChi@users.noreply.github.com>
2023-02-09 14:46:30 +0700

    feat: channel classes (#7)
    * feat: add BaseChannel helper
    
    * feat: handle for channel type

--- src/Managers/ChannelManager.ts
+++ src/Managers/ChannelManager.ts
@@ -1,12 +1,14 @@
-import { APIChannel } from "discord-api-types/v10";
+import { APIChannel, ChannelType } from "discord-api-types/v10";
 import { BaseManager } from "./BaseManager";
 import { Client } from "../Structures/Client";
 import { RedisCollection } from "@nezuchan/redis-collection";
 import { MakeCacheNameFunction } from "../Utilities/Functions";
 import { KeyConstants } from "../Utilities/Enums/KeyConstants";
 import { BaseChannel } from "../Structures/BaseChannel";
+import { VoiceChannel } from "../Structures/VoiceChannel";
+import { TextChannel } from "../Structures/TextChannel";
 
-export class ChannelManager extends BaseManager<APIChannel, BaseChannel> {
+export class ChannelManager extends BaseManager<APIChannel, BaseChannel | TextChannel | VoiceChannel> {
     public constructor(
         client: Client
     ) {
@@ -16,17 +18,25 @@ export class ChannelManager extends BaseManager<APIChannel, BaseChannel> {
         );
     }
 
-    public override makeCache(): RedisCollection<APIChannel, BaseChannel> {
+    public override makeCache(): RedisCollection<APIChannel, BaseChannel | TextChannel | VoiceChannel> {
         return new RedisCollection({
             hash: MakeCacheNameFunction(this.name, this.client.options.clientId!, this.client.options.gatewayRouting ?? false),
             redis: this.client.redis,
-            // TODO: Filter based on channel type.
-            deserialize: value => new BaseChannel(JSON.parse(value as string) as APIChannel, this.client),
+            deserialize: value => {
+                const parsedValue = JSON.parse(value as string) as APIChannel;
+                switch (parsedValue.type) {
+                    case ChannelType.GuildVoice:
+                    case ChannelType.GuildStageVoice:
+                        return new VoiceChannel(parsedValue, this.client);
+                    default:
+                        return new TextChannel(parsedValue, this.client);
+                }
+            },
             serialize: value => JSON.stringify(value)
         });
     }
 
-    public override _patch(old: BaseChannel, data: APIChannel): APIChannel {
+    public override _patch(old: BaseChannel | TextChannel | VoiceChannel, data: APIChannel): APIChannel {
         return data;
     }
 }
--- src/Structures/BaseChannel.ts
+++ src/Structures/BaseChannel.ts
@@ -1,9 +1,41 @@
-import { APIChannel, RESTPostAPIChannelMessageJSONBody } from "discord-api-types/v10";
+import { APIChannel, ChannelFlags, ChannelType, RESTPostAPIChannelMessageJSONBody } from "discord-api-types/v10";
 import { Base } from "./Base";
 import { Message } from "./Message";
+import { TextChannel } from "./TextChannel";
+import { VoiceChannel } from "./VoiceChannel";
 
 export class BaseChannel extends Base<APIChannel> {
+    public get guildId(): string {
+        return this.guildId;
+    }
+
+    public get name(): string {
+        return this.name;
+    }
+
+    public get type(): ChannelType {
+        return this.data.type;
+    }
+
+    public get flags(): ChannelFlags | undefined {
+        return this.data.flags;
+    }
+
     public async send(options: RESTPostAPIChannelMessageJSONBody): Promise<Message> {
         return this.client.sendMessage(options, this.id);
+    }
+
+    public isVoice(): this is VoiceChannel {
+        return this.type === ChannelType.GuildVoice;
+    }
+
+    public isText(): this is TextChannel {
+        return ![
+            ChannelType.GuildStageVoice, ChannelType.GuildVoice
+        ].includes(this.type);
+    }
+
+    public isSendable(): boolean {
+        return ![ChannelType.GuildCategory, ChannelType.GuildDirectory, ChannelType.GuildForum, ChannelType.GuildStageVoice].includes(this.type);
     }
 }
