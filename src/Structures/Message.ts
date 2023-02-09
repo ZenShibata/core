a92c4c46f279ee9b7445bd80650e63c3788d5ecb
kagchi <yonuxbot@gmail.com>
2023-02-09 11:22:23 +0700

    feat: add edit function to message class

--- src/Structures/Message.ts
+++ src/Structures/Message.ts
@@ -1,4 +1,4 @@
-import { APIMessage, GatewayMessageCreateDispatchData } from "discord-api-types/v10";
+import { APIMessage, GatewayMessageCreateDispatchData, RESTPatchAPIChannelMessageJSONBody, Routes } from "discord-api-types/v10";
 import { Base } from "./Base";
 import { Guild } from "./Guild";
 
@@ -11,7 +11,17 @@ export class Message extends Base<APIMessage | GatewayMessageCreateDispatchData>
         return "guild_id" in this.data ? this.data.guild_id : undefined;
     }
 
+    public get channelId(): string {
+        return this.data.channel_id;
+    }
+
     public async fetchGuild(): Promise<Guild | null> {
         return this.guildId ? this.client.guilds.fetch({ id: this.guildId, cache: true }) : null;
+    }
+
+    public async edit(options: RESTPatchAPIChannelMessageJSONBody): Promise<Message> {
+        return this.client.rest.patch(Routes.channelMessage(this.channelId, this.id), {
+            body: options
+        }).then(x => new Message(x as APIMessage, this.client));
     }
 }
