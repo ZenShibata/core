194e2eaa9448130030d59fd024685d2100a2963f
kagchi <yonuxbot@gmail.com>
2023-04-09 17:25:22 +0700

    fix: this is not raw message

--- src/Structures/Interactions/MessageContextMenuInteraction.ts
+++ src/Structures/Interactions/MessageContextMenuInteraction.ts
@@ -2,7 +2,7 @@ import { Message } from "../Message";
 import { BaseContextMenuInteraction } from "./BaseContextMenuInteraction";
 
 export class MessageContextMenuInteraction extends BaseContextMenuInteraction {
-    public getRawMessage(): Message | null {
+    public getMessage(): Message | null {
         return this.data.data && "target_id" in this.data.data && "messages" in this.data.data.resolved ? new Message(this.data.data.resolved.messages[this.data.data.target_id], this.client) : null;
     }
 }
